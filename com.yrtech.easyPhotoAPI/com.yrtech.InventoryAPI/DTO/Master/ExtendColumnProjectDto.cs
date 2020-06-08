72c1ec628304a3d8e14360ecb84ab94757d6a910
Tony <moujunsheng15@163.com>
2020-06-08 23:00:33 +0800

    添加了Dto

---
+++ com.yrtech.easyPhotoAPI/com.yrtech.InventoryAPI/DTO/Master/ExtendColumnProjectDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class ExtendColumnProjectDto
+    {
+        public string ProjectId { get; set; }
+        public string ColumnCode { get; set; }
+        public string ColumnName { get; set; }
+        public Nullable<bool> UseChk { get; set; }
+        public Nullable<bool> AddShowChk { get; set; }
+    }
+}
